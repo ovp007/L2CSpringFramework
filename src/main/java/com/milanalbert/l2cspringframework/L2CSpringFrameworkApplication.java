7fc68fc1c881fb4a4f4b2384b59c2e7b13c56252
ovp007 <m.albertbb@gmail.com>
2022-02-22 17:42:08 +0100

    initial commit

---
+++ src/main/java/com/milanalbert/l2cspringframework/L2CSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.milanalbert.l2cspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2CSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L2CSpringFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milanalbert/l2cspringframework/L2CSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milanalbert.l2cspringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L2CSpringFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
